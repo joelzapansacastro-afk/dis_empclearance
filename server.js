3e9e49b42178ae18526465878b15345b4d29a9a6
JOEL ZAPANSA CASTRO <Joel.Castro@domuscholaph.com>
2026-05-19 09:28:16 +0800

    change PORT

--- server.js
+++ server.js
@@ -331,8 +331,9 @@ app.listen(process.env.PORT, () => {
     console.log("🚀 Server running on port " + process.env.PORT);
 });
 */
-const PORT = process.env.PORT || 10000;
+// ✅ CRITICAL FIX
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
