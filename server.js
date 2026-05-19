fffeab52f58e9312dda43cd12107bbced4fb9271
JOEL ZAPANSA CASTRO <Joel.Castro@domuscholaph.com>
2026-05-19 09:15:32 +0800

    change localhost

--- server.js
+++ server.js
@@ -326,6 +326,13 @@ app.put("/updateClearanceBySignatory", async (req, res) => {
 });
 
 /* ================= SERVER START ================= */
+/*
 app.listen(process.env.PORT, () => {
     console.log("🚀 Server running on port " + process.env.PORT);
+});
+*/
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
